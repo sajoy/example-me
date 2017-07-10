b0257200706a1dbd269bc69f1233b60eaca74ac2
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-07-10 14:15:29 -0700

    ask user to guess a state i lived in; track their total score

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 'use strict';
 
 var username = prompt( 'Hi there! What\'s your name?' );
-console.log( 'Their name is ' + username );
+var score = 0;
+console.log( 'Their name is ' + username + ' and their score is ' + score );
 alert( 'Hello ' + username + '! It\'s nice to meet you. I know we haven\'t met,' 
       + ' but I feel like you know me. Try to guess some facts about me. Ready?' );
+
+
 
 var guessOne = prompt( 'Is pink my favorite color?' ).toLowerCase();
 console.log( 'pink is my favorite color : ' + username + ' guessed ' + guessOne );
 if ( guessOne === 'no' || guessOne === 'n' ) {
     alert( 'Sorry ' + username + '! Pink is my favorite color.' );
 } else if ( guessOne === 'yes' || guessOne === 'y' ) {
+    score++;
     alert( 'Yay ' + username + '! You are right. Pink is my favorite color.' );
 }
 
 var guessTwo = prompt( 'Am I a sports fanatic?' ).toLowerCase();
 console.log( 'am i a sports fanatic : ' + username + ' guessed ' + guessTwo );
 if ( guessTwo === 'no' || guessTwo === 'n' ) {
+    score++;
     alert( 'Yay ' + username + '! You are 100% right.' );
 } else if ( guessTwo === 'yes' || guessTwo === 'y' ) {
     alert( 'Nope ' + username + ', I am not a big sports fan.' );
@@ -24,6 +29,7 @@ if ( guessTwo === 'no' || guessTwo === 'n' ) {
 var guessThree = prompt( 'Am I a dog person?' ).toLowerCase();
 console.log( 'am i a dog person : ' + username + ' guessed ' + guessThree );
 if ( guessThree === 'no' || guessThree === 'n' ) {
+    score++;
     alert( 'That\'s right ' + username + '! I am def a cat person.' );
 } else if ( guessThree === 'yes' || guessThree === 'y' ) {
     alert( 'Sorry ' + username + ', I prefer cats.' );
@@ -34,6 +40,7 @@ console.log( 'do i like to write : ' + username + ' guessed ' + guessFour );
 if ( guessFour === 'no' || guessFour === 'n' ) {
     alert( 'Sorry ' + username + '! I like it so much I majored in it.' );
 } else if ( guessFour === 'yes' || guessFour === 'y' ) {
+    score++;
     alert( 'Yay ' + username + '! You are right. I <3 writing.' );
 }
 
@@ -42,8 +49,11 @@ console.log( 'like pizza : ' + username + ' guessed ' + guessFive );
 if ( guessFive === 'no' || guessFive === 'n' ) {
     alert( '....' + username + '.... who doesn\'t like pizza? WRONG.' );
 } else if ( guessFive === 'yes' || guessFive === 'y' ) {
+    score++;
     alert( 'Of course ' + username + '! Give me a slice of Hawaiian, please.' );
 }
+
+
 
 var totalGuesses = 0;
 for ( var i = 0; i < 4; i ++ ) {
@@ -53,6 +63,7 @@ for ( var i = 0; i < 4; i ++ ) {
     var guessesLeft = 4 - totalGuesses;
 
     if ( numberGuess === 3 ) {
+        score++;
         alert( 'That\'s right!!! I lived in the Philippines for 3 years.' );
         break;
     } else if ( numberGuess < 3 ) {
@@ -62,3 +73,30 @@ for ( var i = 0; i < 4; i ++ ) {
     }
 }
 
+
+
+var states = [ 'california', 'virginia', 'oregon' ];
+var correctGuess = false;
+var stateGuesses = 0;
+while ( correctGuess === false ) {
+    var stateGuess = prompt( 'Guess a state that I\'ve lived in. You have 6 guesses.' ).toLowerCase();
+    console.log( username + ' guessed that you live in ' + stateGuess );
+
+    if ( states.indexOf( stateGuess ) > -1 ) {
+        alert( 'Great job, ' + username + '! I did live in ' + stateGuess 
+                + '. Here are the other states I lived in: ' + states.join(', ') );
+        score++;
+        correctGuess = true;
+    } else {
+        alert( 'Nope ' + username + '! I didn\'t live in ' + stateGuess + '.');
+        stateGuesses++;
+    }
+
+    if ( stateGuesses === 6 ) {
+        alert( 'Sorry ' + username + '! You\'re out of guesses. '
+                + '. Here are the states I lived in: ' + states.join(', ') );
+        break;
+    }
+}
+
+alert( 'Thanks for playing ' + username + '!! You got ' + score + ' questions right!' );
