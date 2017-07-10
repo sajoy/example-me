41e128dd9900140ef0d154ef85b5f4d7809aecae
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-07-10 14:30:38 -0700

    put yes no questions in an array and loop to ask them

--- script.js
+++ script.js
@@ -6,51 +6,52 @@ console.log( 'Their name is ' + username + ' and their score is ' + score );
 alert( 'Hello ' + username + '! It\'s nice to meet you. I know we haven\'t met,' 
       + ' but I feel like you know me. Try to guess some facts about me. Ready?' );
 
+var questions = [
+    [
+        'Is pink my favorite color?',
+        'y',
+        'Sorry ' + username + '! Pink is my favorite color.',
+        'Yay ' + username + '! You are right. Pink is my favorite color.'],
+    [
+        'Am I a sports fanatic?',
+        'n',
+        'Nope ' + username + ', I am not a big sports fan.', 
+        'Yay ' + username + '! You are 100% right.'
+    ],
+    [
+        'Am I a dog person?',
+        'n',
+        'Sorry ' + username + ', I prefer cats.',
+        'That\'s right ' + username + '! I am def a cat person.'
+    ],
+    [
+        'Do I like to write?',
+        'y',
+        'Sorry ' + username + '! I like it so much I majored in it.',
+        'Yay ' + username + '! You are right. I <3 writing.'
+    ],
+    [
+        'Do I like pizza?',
+        'y',
+        '....' + username + '.... who doesn\'t like pizza? WRONG.',
+        'Of course ' + username + '! Give me a slice of Hawaiian, please.'
+    ]
+];
 
+for ( var i = 0; i < questions.length; i ++ ) {
+    var question = questions[i][0];
+    var correctAnswer = questions[i][1];
+    var badReply = questions[i][2];
+    var correctReply = questions[i][3];
 
-var guessOne = prompt( 'Is pink my favorite color?' ).toLowerCase();
-console.log( 'pink is my favorite color : ' + username + ' guessed ' + guessOne );
-if ( guessOne === 'no' || guessOne === 'n' ) {
-    alert( 'Sorry ' + username + '! Pink is my favorite color.' );
-} else if ( guessOne === 'yes' || guessOne === 'y' ) {
-    score++;
-    alert( 'Yay ' + username + '! You are right. Pink is my favorite color.' );
-}
-
-var guessTwo = prompt( 'Am I a sports fanatic?' ).toLowerCase();
-console.log( 'am i a sports fanatic : ' + username + ' guessed ' + guessTwo );
-if ( guessTwo === 'no' || guessTwo === 'n' ) {
-    score++;
-    alert( 'Yay ' + username + '! You are 100% right.' );
-} else if ( guessTwo === 'yes' || guessTwo === 'y' ) {
-    alert( 'Nope ' + username + ', I am not a big sports fan.' );
-}
-
-var guessThree = prompt( 'Am I a dog person?' ).toLowerCase();
-console.log( 'am i a dog person : ' + username + ' guessed ' + guessThree );
-if ( guessThree === 'no' || guessThree === 'n' ) {
-    score++;
-    alert( 'That\'s right ' + username + '! I am def a cat person.' );
-} else if ( guessThree === 'yes' || guessThree === 'y' ) {
-    alert( 'Sorry ' + username + ', I prefer cats.' );
-}
-
-var guessFour = prompt( 'Do I like to write?' ).toLowerCase();
-console.log( 'do i like to write : ' + username + ' guessed ' + guessFour );
-if ( guessFour === 'no' || guessFour === 'n' ) {
-    alert( 'Sorry ' + username + '! I like it so much I majored in it.' );
-} else if ( guessFour === 'yes' || guessFour === 'y' ) {
-    score++;
-    alert( 'Yay ' + username + '! You are right. I <3 writing.' );
-}
-
-var guessFive = prompt( 'Do I like pizza?' ).toLowerCase();
-console.log( 'like pizza : ' + username + ' guessed ' + guessFive );
-if ( guessFive === 'no' || guessFive === 'n' ) {
-    alert( '....' + username + '.... who doesn\'t like pizza? WRONG.' );
-} else if ( guessFive === 'yes' || guessFive === 'y' ) {
-    score++;
-    alert( 'Of course ' + username + '! Give me a slice of Hawaiian, please.' );
+    var guess = prompt( question ).toLowerCase()[0];
+    console.log( question + ' : ' + guess );
+    if ( guess === correctAnswer ) {
+        alert( correctReply );
+        score++;
+    } else {
+        alert( badReply );
+    }
 }
 
 
